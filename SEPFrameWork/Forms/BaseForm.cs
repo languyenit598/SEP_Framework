8100e91c5695528c63d9ecbc54ea1e1cdd6b7af9
Nguyễn Hữu Hòa <nguyenhuuhoa1998@gmail.com>
2019-12-21 15:55:30 +0700

    init

--- SEPFrameWork/Forms/BaseForm.cs
+++ SEPFrameWork/Forms/BaseForm.cs
@@ -12,9 +12,16 @@ namespace SEPFrameWork.Forms
 {
     public partial class BaseForm : Form
     {
+        private Panel pn1;
+
         public BaseForm()
         {
             InitializeComponent();
+            pn1 = new Panel();
+            pn1.Size = new Size(200, 100);
+            pn1.BackColor = Color.Red;
+            pn1.Location = new Point(0, 0);
+            this.Controls.Add(pn1);
         }
     }
 }
