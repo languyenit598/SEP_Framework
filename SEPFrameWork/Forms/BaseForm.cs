7d05b781d9f56195f0a921f579e1df1ae0d5682e
Nguyễn Hữu Hòa <nguyenhuuhoa1998@gmail.com>
2020-01-05 07:25:22 +0700

    finish base  form

--- SEPFrameWork/Forms/BaseForm.cs
+++ SEPFrameWork/Forms/BaseForm.cs
@@ -30,19 +30,25 @@ namespace SEPFrameWork.Forms
         private string databaseName, tableName;
         private IConnector databaseConnection = null;
         private List<string> fields = null;
+        private List<string> fieldsAuto = null;
+        private List<string> fieldsPrimary = null;
 
         #region Load
         private void Load()
         {
             fields = databaseConnection.GetNameFieldsOfTable(tableName);
+            fieldsAuto = databaseConnection.GetFieldsAutoIncrement(tableName);
+            fieldsPrimary = databaseConnection.GetPrimaryKeyOfTable(tableName);
             int x = 50, y = 100;
             int idx = 0;
+            ToolTip tt = new ToolTip();
+
             foreach (var field in fields)
             {
                 // Add label
                 Label lbl = new Label();
                 lbl.Text = field;
-                lbl.Size = new Size(200, 30);
+                lbl.Size = new Size(180, 30);
                 lbl.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
                 lbl.Location = new System.Drawing.Point(x , y + idx*50);
                 lbl.Name = "lbl"+idx;
@@ -52,9 +58,37 @@ namespace SEPFrameWork.Forms
                 TextBox txt = new TextBox();
                 txt.Text = "";
                 txt.Size = new Size(300, 30);
-                txt.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+                txt.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
                 txt.Location = new System.Drawing.Point(x + 250, y + idx * 50);
                 txt.Name = "txt" + idx;
+
+                if (fieldsAuto.Contains(field, StringComparer.OrdinalIgnoreCase))
+                {
+                    txt.Enabled = false;
+                    txt.ForeColor = Color.Red;
+                    PictureBox pbAuto = new PictureBox();
+                    pbAuto.Size = new Size(25, 25);
+                    pbAuto.Location = new System.Drawing.Point(x + 210, y + idx * 50);
+                    Bitmap pic1 = new Bitmap(Application.StartupPath + "\\Icon\\autoKey.png");
+                    pbAuto.Image = pic1;
+                    pbAuto.SizeMode = PictureBoxSizeMode.StretchImage;
+                    tt.SetToolTip(pbAuto, "Thuộc tính tự tăng, không thể sửa giá trị");
+                    this.Controls.Add(pbAuto);
+                }
+                if (fieldsPrimary.Contains(field,StringComparer.OrdinalIgnoreCase))
+                {
+                    lbl.ForeColor = Color.Red;
+                    txt.Enabled = false;
+                    txt.ForeColor = Color.Red;
+                    PictureBox pbPrimary = new PictureBox();
+                    pbPrimary.Size = new Size(25, 25);
+                    pbPrimary.Location = new System.Drawing.Point(x + 180, y + idx * 50);
+                    Bitmap pic2 = new Bitmap(Application.StartupPath + "\\Icon\\primaryKey.png");
+                    pbPrimary.Image = pic2;
+                    pbPrimary.SizeMode = PictureBoxSizeMode.StretchImage;
+                    tt.SetToolTip(pbPrimary, "Thuộc tính primary, không thể sửa giá trị");
+                    this.Controls.Add(pbPrimary);
+                }
                 this.Controls.Add(txt);
 
                 idx++;
