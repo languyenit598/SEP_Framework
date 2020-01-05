0f385bad296f8f9c08cd14c2ad144d2111de6a54
Nguyễn Hữu Hòa <nguyenhuuhoa1998@gmail.com>
2020-01-06 01:11:38 +0700

    add factory class

---
+++ SEPFrameWork/AddFormFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SEPFrameWork.Forms;
+
+namespace SEPFrameWork
+{
+    class AddFormFactory : FactoryForm
+    {
+        public override BaseForm creat()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SEPFrameWork/FactoryForm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SEPFrameWork.Forms;
+
+namespace SEPFrameWork
+{
+    abstract class FactoryForm
+    {
+        public abstract BaseForm creat();
+    }
+}
