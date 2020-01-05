4077a4bf8c802db47a69a69a9497724526af5459
Nguyễn Hữu Hòa <nguyenhuuhoa1998@gmail.com>
2020-01-05 12:01:05 +0700

    update dữ liệu

--- SEPFrameWork/Forms/UpdateForm.cs
+++ SEPFrameWork/Forms/UpdateForm.cs
@@ -27,11 +27,37 @@ namespace SEPFrameWork.Forms
             base.windowsName = windowsName;
             base.obj = obj;
             this.Text = base.windowsName; // override lại tên
+            // set dài rộng cửa sổ
+            int sz = base.fields.Count;
+            this.Size = new Size(713, 100 + sz * 50 + 100);
         }
 
         protected override void doSomething()
         {
             // code here
+            int idx = 0;
+            int sz = base.fields.Count;
+            Object[] obj = null;
+            if (base.fieldsAuto.Count > 0) // có 1 dòng tự tăng (auto increment) => bỏ qua dòng đó k truyền
+            {
+                obj = new Object[sz - 1]; // bỏ qua cột auto
+                idx++; // lấy từ txt1.Text
+            }
+            else
+            {
+                obj = new Object[sz]; // bỏ qua cột auto
+                // Lấy từ txt0.Text
+            }
+            int idxObj = 0;
+
+            foreach (var field in base.fields)
+            {
+                obj[idxObj] = getDataTextBox("txt" + idx.ToString()); // object[0]=txt0.Text ......
+                idxObj++;
+                idx++;
+            }
+            base.databaseConnection.UpdateData(base.tableName, base.obj, this.obj);
+            MessageBox.Show("Cập nhật dữ liệu thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
 
         }
     }
